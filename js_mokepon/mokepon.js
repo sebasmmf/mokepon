feed38b522bf5c7e9a9ac0c422ac62ce39f24f46
Sebastian Martinez <sebastian.mart.f@gmail.com>
2023-09-20 21:52:17 -0500

    First commit

---
+++ js_mokepon/mokepon.js
@@ -0,0 +1,67 @@
+let ataqueJugador
+
+function iniciarJuego() {
+  let botonMascotaJugador = document.getElementById('boton-mascota')
+  botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+  let botonFuego = document.getElementById('boton-fuego')
+  botonFuego.addEventListener('click',ataqueFuego)
+  let botonAgua = document.getElementById('boton-agua')
+  botonAgua.addEventListener('click',ataqueAgua)
+  let botonTierra = document.getElementById('boton-tierra')
+  botonTierra.addEventListener('click',ataqueTierra)
+}
+
+function seleccionarMascotaJugador() {
+  let inputHipudoge = document.getElementById('hipodoge')
+  let inputCapipepo = document.getElementById('capipepo')
+  let inputRatigueya = document.getElementById('ratigueya')
+  let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+  if (inputHipudoge.checked) {
+    spanMascotaJugador.innerHTML = 'Hipodoge'
+  } else if (inputCapipepo.checked) {
+    spanMascotaJugador.innerHTML = 'Capipepo'
+  } else if (inputRatigueya.checked) {
+    spanMascotaJugador.innerHTML = 'Ratigueya'
+  } else {
+    alert('Selecciona una mascota')
+  }
+
+  seleccionarMascotaEnemigo()
+
+}
+
+function seleccionarMascotaEnemigo() {
+  let ataqueAleatorio = aleatorio(1,3)
+  spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+  if (ataqueAleatorio == 1) {
+    spanMascotaEnemigo.innerHTML = 'Hipodoge'
+  } else if (ataqueAleatorio == 2) {
+    spanMascotaEnemigo.innerHTML = 'Capipepo'
+  } else {
+    spanMascotaEnemigo.innerHTML = 'Ratigueya'
+  }
+}
+
+function ataqueFuego() {
+  ataqueJugador = 'FUEGO'
+  alert(ataqueJugador)
+}
+
+function ataqueAgua() {
+  ataqueJugador = 'AGUA'
+  alert(ataqueJugador)
+}
+
+function ataqueTierra() {
+  ataqueJugador = 'TIERRA'
+  alert(ataqueJugador)
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+window.addEventListener('load', iniciarJuego)
